6ec441d8c89362fd9626b6e56e549c1cdf5da4dc
RobertoCiurea <robertociureaionut@gmail.com>
2023-09-02 14:29:50 +0300

    fix config for google profile image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.example.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
