92a3d3e6cfcc520315d2f30d85d0418f10a80474
RobertoCiurea <robertociureaionut@gmail.com>
2023-10-07 16:54:50 +0300

    moved async function to server component

--- app/create-ad/page.tsx
+++ app/create-ad/page.tsx
@@ -1,39 +1,6 @@
+import React from "react";
 import { ClientForm } from "@/components/index";
-import { connectToDatabase } from "@/db";
-import { adsModel } from "@/models/Ads";
-import { AdsProps } from "@/types/index";
-
-//send data to database
-async function sendDataToDatabase(formData: FormData, tags: string[]) {
-  "use server";
-  try {
-    connectToDatabase();
-    console.log("Connected to database");
-    const newAd: AdsProps = new adsModel({
-      title: formData.get("title"),
-      salary: formData.get("salary"),
-      owner: formData.get("owner"),
-      description: formData.get("description"),
-      email: formData.get("email"),
-      phone: formData.get("phone"),
-      location: formData.get("location"),
-      tags: tags,
-      createdAt: new Date(),
-    });
-    newAd.save();
-    console.log(newAd);
-
-    return {
-      status: 201,
-      body: { message: "Ad created successfully" },
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      body: { message: "An error occurred while creating the ad" },
-    };
-  }
-}
+import { sendDataToDatabase } from "@/utilities/sendDataToDatabase";
 const page = () => {
   return (
     //outer div
@@ -41,7 +8,8 @@ const page = () => {
       <h1 className="font-Manrope font-bold text-4xl py-10">
         Create your <span className="text-primary">ad</span>
       </h1>
-      <ClientForm {...{ sendDataToDatabase }} />
+
+      <ClientForm sendDataToDatabase={sendDataToDatabase} />
     </div>
   );
 };
